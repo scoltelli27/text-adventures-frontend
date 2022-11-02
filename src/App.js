ba2e9819817d4289c0f6787ff3042da35a3628a1
scoltelli27 <92338065+scoltelli27@users.noreply.github.com>
2022-11-02 17:42:03 -0300

    Designing the UI

--- src/App.js
+++ src/App.js
@@ -12,35 +12,45 @@ import EditStory from "./features/stories/EditStory";
 import NewStory from "./features/stories/NewStory";
 import Prefetch from "./features/auth/Prefetch";
 import PersistLogin from "./features/auth/PersistLogin";
-import React from "react";
+import RequireAuth from "./features/auth/RequireAuth";
+import { ROLES } from "./config/roles";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* Public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
+        {/* Protected routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<Prefetch />}>
-            <Route path="dash" element={<DashLayout />}>
-              <Route index element={<Welcome />} />
+          <Route
+            element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+          >
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
+                <Route index element={<Welcome />} />
 
-              <Route path="users">
-                <Route index element={<UsersList />} />
-                <Route path=":id" element={<EditUser />} />
-                <Route path="new" element={<NewUserForm />} />
-              </Route>
+                <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
 
-              <Route path="stories">
-                <Route index element={<StoriesList />} />
-                <Route path=":id" element={<EditStory />} />
-                <Route path="new" element={<NewStory />} />
+                <Route path="stories">
+                  <Route index element={<StoriesList />} />
+                  <Route path=":id" element={<EditStory />} />
+                  <Route path="new" element={<NewStory />} />
+                </Route>
               </Route>
-            </Route>{" "}
-            {/* End Dash */}
+              {/* End Dash */}
+            </Route>
           </Route>
         </Route>
+        {/* End protected routes */}
       </Route>
     </Routes>
   );
--- src/components/Public.js
+++ src/components/Public.js
@@ -1,28 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.svg";
 
 const Public = () => {
-    const content = (
-        <section className="public">
-            <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
-            </header>
-            <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-                <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
-                <br />
-                <p>Owner: Dan Davidson</p>
-            </main>
-            <footer>
-                <Link to="/login">Employee Login</Link>
-            </footer>
-        </section>
-
-    )
-    return content
-}
-export default Public
+  const content = (
+    <section className="public">
+      <navbar className="navbar">
+        <ul className="flex">
+          <li>
+            <img src={Logo} className="logo" alt="logo" />
+          </li>
+          <li>
+            <span>Text adventures</span>
+          </li>
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+        </ul>
+      </navbar>
+      <main className="public__main">
+        <h2 className="titles">DECIDE YOUR FATE</h2>
+        <h1 className="titles">CHOOSE YOUR OWN ADVENTURE</h1>
+        <h3 className="titles">FACE THE CONSEQUENCES</h3>
+        <div className="buttons flex">
+          <button className="main__button">Read</button>
+          <button className="main__button">Write</button>
+        </div>
+      </main>
+    </section>
+  );
+  return content;
+};
+export default Public;
